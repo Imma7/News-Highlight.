9022c1670c639aa061f47e8570783de57bf7a16c
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-18 15:15:57 +0300

    Added source and article base url

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request, json
+from .models import news
+
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the news base url
+base_source_url = app.config['NEWS_SOURCE_API_BASE_URL']
+base_article_url = app.config['NEWS_ARTICLES_API_BASE_URL']

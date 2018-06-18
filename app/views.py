d12218f12da9bbdedef37765b6c902e0dacb3ffb
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-18 11:31:02 +0300

    Storing API Keys

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = "Welcome to News Highlight"
-    return render_template('index.html', message = message)
+    title = "Welcome to News Highlight"
+    return render_template('index.html', title = title)
 
 # @app.route('/articles/<articles_id>')
 # def articles(articles_id):

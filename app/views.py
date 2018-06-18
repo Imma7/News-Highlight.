c70c45275657a9353884b79d252412bafcb3197c
Immanuel Mugambi <immamugambi@gmail.com>
2018-06-18 11:17:20 +0300

    Created configuration classes

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from app import app
 
-#Views
-app.route('/')
+# Views
+@app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = "Welcome to News Highlight"
+    return render_template('index.html', message = message)
+
+# @app.route('/articles/<articles_id>')
+# def articles(articles_id):
+#     '''
+#     View articles page function that returns artcicles details and its data
+#     '''
+#     return render_template('articles.html')
